7cce01d1bbf3f05847b4a4434984416fa975f4bd
Josh Watts <josh+github@sroz.net>
2014-04-23 20:15:09 -0400

    Split track data into multiple object files
    This original goal of this commit was to reduce the time required to
    compile all of the tracks. While it now takes *longer* on my Core i7 Duo,
    my CPUs are now actually maxed at 100%, and disk I/O is fully saturated!
    
    An added benefit, is that this should alleviate memory related crashes in
    Brandon's compiler, as the compiler only has to deal w/ a handful of ~4MiB
    and ~70MiB source files instead of a single ~200MiB source file.

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
 netcat-objs += netcat_main.o
+netcat-objs += tracks/trk1.o
+netcat-objs += tracks/trk2.o
+netcat-objs += tracks/trk3.o
+netcat-objs += tracks/trk4.o
+netcat-objs += tracks/trk5.o
+netcat-objs += tracks/trk6.o
 obj-m += netcat.o
 KERNELDIR	?=  /lib/modules/$(shell uname -r)/build
 PWD		:= $(shell pwd)
---
+++ tracks/trk2.c
@@ -0,0 +1,15 @@
+#define NETCAT_CPI_TRK2_LEN 11659615
+static char netcat_cpi_trk2[] = {
+
+#include "trk2data.h"
+
+};
+
+static char *netcat_cpi_trk2_name = "The Internet is an Apt Motherfucker";
+
+void netcat_cpi_register_trk2(char **trk, char **name, unsigned long *len)
+{
+	*trk = netcat_cpi_trk2;
+	*name = netcat_cpi_trk2_name;
+	*len = NETCAT_CPI_TRK2_LEN;
+}
