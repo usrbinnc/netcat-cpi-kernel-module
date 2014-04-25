f2bebe6e97e3a0346b01da4bb84b15a9b6448eec
Philipp A. Hartmann <pah@qo.cx>
2014-04-25 07:42:51 +0200

    move external track symbols to netcat.h
    Extern symbols are frowned upon in .c files.
    
    Signed-off-by: Philipp A. Hartmann <pah@qo.cx>

--- netcat.h
+++ netcat.h
@@ -4,4 +4,12 @@ struct netcat_track {
 	unsigned long len;
 	char data[];
 };
+
+extern struct netcat_track netcat_cpi_trk1;
+extern struct netcat_track netcat_cpi_trk2;
+extern struct netcat_track netcat_cpi_trk3;
+extern struct netcat_track netcat_cpi_trk4;
+extern struct netcat_track netcat_cpi_trk5;
+extern struct netcat_track netcat_cpi_trk6;
+
 #endif /* NETCAT_H_INCLUDED_ */
